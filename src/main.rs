6e8d09e44d56eff6ee85c7083fa8c83574964abd
Michael Vessia <michael@vessia.net>
2022-10-10 23:58:22 -0400

    is_valid_word?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+const WORD_LENGTH: usize = 5;
+
 pub fn sanitize_word(word: &str) -> String {
     word.trim()
         .to_uppercase()
         .chars()
         .filter(|c| c.is_ascii_alphabetic())
         .collect()
+}
+
+pub fn is_valid_word(word: &str) -> bool {
+    return word.len() == WORD_LENGTH;
 }
 
 #[cfg(test)]
@@ -19,5 +25,13 @@ mod tests {
         assert_eq!(sanitize_word("HELLO  "), "HELLO");
         assert_eq!(sanitize_word("HEL  LO"), "HELLO");
         assert_eq!(sanitize_word("H3L\nL0"), "HLL");
+    }
+
+    #[test]
+    fn test_is_valid_word() {
+        assert_eq!(is_valid_word("HELLO"), true);
+        assert_eq!(is_valid_word("WORLD"), true);
+        assert_eq!(is_valid_word("WRLD"), false);
+        assert_eq!(is_valid_word("WOORLD"), false);
     }
 }
